c9dd43138af8d86c5bc3fbaba0df2974cf90257f
Luca Marchiorello <marchiorelloluca@gmail.com>
2015-09-02 09:44:43 +0000

    fixed mail result

--- contacts.php
+++ contacts.php
@@ -32,7 +32,6 @@
 if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 	if (mail ($to, $subject, $body, $from)) {
 		$result='<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Thank You!</strong> I will be in touch</div>';
-		$result= '<p>'+ $to +'</p>'+ $result;
 	} else {
 		$result='<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Sorry there was an <strong>error</strong> sending your message. Please write at lucamarkio@hotmail.com </div>';
 	}
