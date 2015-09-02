e5959077934040409e52c880968e99adcc6b33d1
Luca Marchiorello <marchiorelloluca@gmail.com>
2015-09-02 09:58:03 +0000

    added jquery and bootstrap.js for dismissible alerts

--- contacts.php
+++ contacts.php
@@ -136,8 +136,9 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
         <p class="text-muted">Copyright &copy; <?php echo date("Y"); ?> - Luca Marchiorello - All rights reserved.</p>
       </div>
     </footer>
-
-
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
--- index.php
+++ index.php
@@ -94,6 +94,8 @@
     </footer>
 
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
     <script>
